e5dc8355a0c483fadcf7daa657e93f961c2ce90f
David Lee <jolee211@icloud.com>
2021-02-01 14:58:47 -0700

    Allow previewing the UI with Fixed Layout

--- PlaceDescription/ContentView.swift
+++ PlaceDescription/ContentView.swift
@@ -13,11 +13,12 @@ struct LabelTextField : View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(label).font(.headline)
-            TextField("", text: $placeHolder)
+            Text(label).font(.headline).lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+            Text(placeHolder)
                 .padding(.all)
                 .background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
                 .cornerRadius(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
+                .lineLimit(3)
         }
         .padding(.horizontal, 15)
     }
@@ -28,12 +29,25 @@ struct ContentView: View {
 
     
     var body: some View {
-        LabelTextField(label: "Name", placeHolder: placeDescription.name)
+        List {
+            VStack(alignment: .leading) {
+                LabelTextField(label: "Name", placeHolder: placeDescription.name)
+                LabelTextField(label: "Description", placeHolder: placeDescription.description)
+                LabelTextField(label: "Category", placeHolder: placeDescription.category)
+                LabelTextField(label: "Address Title", placeHolder: placeDescription.address.title)
+                LabelTextField(label: "Address", placeHolder: placeDescription.address.street + "\n" + placeDescription.address.city + ", " + placeDescription.address.state + " " + placeDescription.address.zipCode + "\n" + placeDescription.address.country)
+                LabelTextField(label: "Elevation", placeHolder: String(format: "%.1f", placeDescription.elevation))
+                LabelTextField(label: "Latitude", placeHolder: String(format: "%.6f", placeDescription.latitude))
+                LabelTextField(label: "Longitude", placeHolder: String(format: "%.6f", placeDescription.longitude))
+            }
+            .listRowInsets(EdgeInsets())
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 375, height: 1000))
     }
 }

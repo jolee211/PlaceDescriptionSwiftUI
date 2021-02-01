628e59f84fa8423283a902eea6627e65c0f788d0
David Lee <jolee211@icloud.com>
2021-02-01 08:09:52 -0700

    first commit

---
+++ PlaceDescription/PlaceDescriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceDescriptionApp.swift
+//  PlaceDescription
+//
+//  Created by Joseph Lee on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct PlaceDescriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

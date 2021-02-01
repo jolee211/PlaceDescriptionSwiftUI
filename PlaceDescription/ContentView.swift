7340333871995c16b6fc0ee222a0888cbc172bed
David Lee <jolee211@icloud.com>
2021-02-01 14:32:30 -0700

    Created a reusable Text Field

--- PlaceDescription/ContentView.swift
+++ PlaceDescription/ContentView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LabelTextField : View {
+    var label: String
+    @State var placeHolder: String
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text(label).font(.headline)
+            TextField("", text: $placeHolder)
+                .padding(.all)
+                .background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
+                .cornerRadius(/*@START_MENU_TOKEN@*/5.0/*@END_MENU_TOKEN@*/)
+        }
+        .padding(.horizontal, 15)
+    }
+}
+
+struct ContentView: View {
+    @State private var placeDescription = PlaceDescription(name: "Home", description: "Home", category: "residence", address: Address(title: "Joseph Lee", street: "1741 W Flamingo Dr", city: "Chandler", state: "AZ", country: "US", zipCode: "85286"), elevation: 1384.0, latitude: 33.27532, longitude: -111.87144)
+
+    
+    var body: some View {
+        LabelTextField(label: "Name", placeHolder: placeDescription.name)
     }
 }
 

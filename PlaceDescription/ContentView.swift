c58aba8ab84f95bfee64e48749b1dba397d5a652
David Lee <jolee211@icloud.com>
2021-02-02 15:57:49 -0700

    Construct PlaceDescription with JSON string

--- PlaceDescription/ContentView.swift
+++ PlaceDescription/ContentView.swift
@@ -25,22 +25,11 @@ class PlaceDescription {
     var category: String
     var addressTitle: String
     var addressStreet: String
-    var elevation: Float
-    var latitude: Float
-    var longitude: Float
+    var elevation: Double
+    var latitude: Double
+    var longitude: Double
     
-    init(json: [String: Any]) {
-        name = json["name"] as? String ?? ""
-        description = json["description"] as? String ?? ""
-        category = json["category"] as? String ?? ""
-        addressTitle = json["address-title"] as? String ?? ""
-        addressStreet = json["address-street"] as? String ?? ""
-        elevation = json["elevation"] as? Float ?? 0
-        latitude = json["latitude"] as? Float ?? 0
-        longitude = json["longitude"] as? Float ?? 0
-    }
-    
-    init(jsonStr: String) {
+    init() {
         self.name = ""
         self.description = ""
         self.category = ""
@@ -49,11 +38,28 @@ class PlaceDescription {
         self.elevation = 0
         self.latitude = 0
         self.longitude = 0
+    }
+    
+    convenience init(json: [String: Any]) {
+        self.init()
+        
+        name = json["name"] as? String ?? ""
+        description = json["description"] as? String ?? ""
+        category = json["category"] as? String ?? ""
+        addressTitle = json["address-title"] as? String ?? ""
+        addressStreet = json["address-street"] as? String ?? ""
+        elevation = json["elevation"] as? Double ?? 0
+        latitude = json["latitude"] as? Double ?? 0
+        longitude = json["longitude"] as? Double ?? 0
+    }
+    
+    convenience init(jsonStr: String) {
+        self.init()
         
         if let data: Data = jsonStr.data(using: String.Encoding.utf8) {
             do {
-                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-                print(json)
+                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+                self.init(json: json)
             } catch let jsonErr {
                 print("Error serializing JSON:", jsonErr)
             }
@@ -79,28 +85,74 @@ struct LabelTextField : View {
     }
 }
 
+var placeDescription = PlaceDescription()
+
 struct ContentView: View {
-    @State private var placeDescription = PlaceDescription(jsonStr: "{\"name\": \"ASU-Poly\",\"description\": \"Home of ASU\'s Software Engineering Programs\", \"category\": \"School\", \"address-title\": \"ASU Software Engineering\", \"address-street\": \"7171 W Sonoran Arroyo Mall\\nPeralta Hall 230\\nMesa AZ 85212\", \"elevation\": 1384.0, \"latitude\": 33.306388, \"longitude\": -111.679121}")
+    @State var jsonStr: String = ""
     
     var body: some View {
-        List {
-            VStack(alignment: .leading) {
-                LabelTextField(label: "Name", placeHolder: placeDescription.name)
-                LabelTextField(label: "Description", placeHolder: placeDescription.description)
-                LabelTextField(label: "Category", placeHolder: placeDescription.category)
-                LabelTextField(label: "Address Title", placeHolder: placeDescription.addressTitle)
-                LabelTextField(label: "Address Street", placeHolder: placeDescription.addressStreet)
-                LabelTextField(label: "Elevation", placeHolder: String(format: "%.1f", placeDescription.elevation))
-                LabelTextField(label: "Latitude", placeHolder: String(format: "%.6f", placeDescription.latitude))
-                LabelTextField(label: "Longitude", placeHolder: String(format: "%.6f", placeDescription.longitude))
+        NavigationView {
+            VStack {
+                Form {
+                    Section {
+                        TextField("Enter JSON", text: $jsonStr)
+                            .lineLimit(3)
+                    }
+                }
+                
+                Button(action: {
+                    placeDescription = PlaceDescription(jsonStr: jsonStr)
+                }, label: {
+                    Text("Init PlaceDescription with JSON")
+                        .frame(width: 300, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                })
+                .padding()
+                
+                NavigationLink(
+                    destination: PlaceDescriptionView(),
+                    label: {
+                        DisplayPlaceDescriptionButton(color: .green)
+                    })
             }
-            .listRowInsets(EdgeInsets())
+            .navigationTitle("Place Description JSON")
         }
+    }
+}
+
+struct PlaceDescriptionView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            LabelTextField(label: "Name", placeHolder: placeDescription.name)
+            LabelTextField(label: "Description", placeHolder: placeDescription.description)
+            LabelTextField(label: "Category", placeHolder: placeDescription.category)
+            LabelTextField(label: "Address Title", placeHolder: placeDescription.addressTitle)
+            LabelTextField(label: "Address Street", placeHolder: placeDescription.addressStreet)
+            LabelTextField(label: "Elevation", placeHolder: String(format: "%.1f", placeDescription.elevation))
+            LabelTextField(label: "Latitude", placeHolder: String(format: "%.6f", placeDescription.latitude))
+            LabelTextField(label: "Longitude", placeHolder: String(format: "%.6f", placeDescription.longitude))
+        }
+        .navigationTitle("PlaceDescription")
+    }
+}
+
+struct DisplayPlaceDescriptionButton: View {
+    let color: Color
+    
+    var body: some View {
+        Text("Display PlaceDescription")
+            .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .background(color)
+            .foregroundColor(.white)
+            .cornerRadius(8)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().previewLayout(.fixed(width: 375, height: 1000))
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
